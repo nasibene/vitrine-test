60cac32220f99e310b9310fdd155f7633da6ef8a
nasibene <nasibenedicte@gmail.com>
2025-04-29 17:01:44 +0200

    ajout import router page accueil

--- site-vitrine-angular/src/app/pages/accueil/accueil.component.ts
+++ site-vitrine-angular/src/app/pages/accueil/accueil.component.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss',
 })
